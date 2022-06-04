4c4b6869b6f6d45191fab8a44ed23de785ac7ab2
Samantha Huang <samanthahuang@berkeley.edu>
2022-06-04 00:47:01 -0700

    Set basic color palette and website layout
    added framework for all text, included  typing effect, selected main colors

---
+++ js/typing.js
@@ -0,0 +1,41 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Programmer", "Designer", "Dog Lover", "Cap Fanatic", "Animation Ethusiast"];
+const typingDelay = 175; //time to type phrase
+const erasingDelay = 100; //time to erase phrase
+const newTextDelay = 2000; // the btwn current and next text
+let textArrayIndex = 0; //reference for which phrase in array u r on
+let charIndex = 0; //reference for which character of a phrase u r on
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) { //check still have char in phrase
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
